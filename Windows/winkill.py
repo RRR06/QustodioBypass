e279f5b771eac9076c5e46f1e82353e5fd677899
jjvfood <jjvfood@gmail.com>
2020-05-12 22:35:41 -0400

    misc cfixes

---
+++ Windows/winkill.py
@@ -0,0 +1,6 @@
+# This is not complete
+# do not fucking even run it, it messes with taskkill and if shit goes south
+# it goes all the way south
+# 
+import os
+os.system("taskkill /f /im  Qustodio_app.exe")
---
+++ Windows/winkillbat.bat
@@ -0,0 +1,1 @@
+Taskkill /f /im [taskname]
