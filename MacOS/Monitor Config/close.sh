3db222c3d0c45fbfb5a9ee7cb02907858a865899
RRR06 <55823984+RRR06@users.noreply.github.com>
2020-05-13 21:21:13 +0200

    Cleanup

--- MacOS/Monitor Config/close.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/zsh
- ECHO 
- ECHO Disabling Qustodio. A restart must happen, please save anything before executing, any unsaved data will be lost. Admin privileges required
- sudo launchctl disable system/com.qustodio.engine
- sudo launchctl disable system/com.qustodio.update
- ECHO 
- ECHO Restarting
- sudo shutdown -r now 
