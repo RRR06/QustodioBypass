5b2fcbb0f98ea73ad10773f5423351d900cf9a19
RRR06 <55823984+RRR06@users.noreply.github.com>
2020-05-13 21:21:30 +0200

    Cleanup

--- MacOS/Monitor Config/open.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/zsh
- ECHO 
- ECHO Enabling Qustodio. A restart must happen, please save anything before executing, any unsaved data will be lost. Admin privileges required
- sudo launchctl enable system/com.qustodio.engine
- sudo launchctl enable system/com.qustodio.update
- ECHO 
- ECHO Restarting
- sudo shutdown -r now 
