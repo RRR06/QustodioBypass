279bd6e8d2366bc12128fc86f3147edbaa513b6f
jjvfood <jjvfood@gmail.com>
2020-05-12 23:10:00 -0400

    fixed my file

--- Windows/winkillbat.bat
+++ Windows/winkillbat.bat
@@ -1,1 +1,3 @@
-Taskkill /f /im [taskname]
+::this is completly non functional at the time please keep your eyes peeled for functional versions
+Taskkill /f /im QUpdateService.exe
+Taskkill /f /im qengine.exe
